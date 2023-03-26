92d7a4f9def0e31713eb89573143ad14556de4be
Afsal <122087993+DarthC0d3r@users.noreply.github.com>
2023-03-26 21:59:53 +0530

    Update AQ2.js

--- AQ2.js
+++ AQ2.js
@@ -8,15 +8,13 @@ for ( i = 0; i < size; i++ ) {
     arr.push( prompt('Enter a number: ') )
 }
 
-
 function replaceEven(size,arr) {
     for ( i = 0; i < size; i++ ) {
         if ( arr[i] %2 === 0 ) {
             arr[i] = `$`
         }
         console.log( arr[i])
-    }
-    
+    }   
 }
 
 replaceEven(size,arr)
