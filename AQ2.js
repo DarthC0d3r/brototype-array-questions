c7d25b02bd6c31e88343adcc09b071be60a2db3b
Afsal <122087993+DarthC0d3r@users.noreply.github.com>
2023-03-26 22:24:17 +0530

    Create AQ2.js

---
+++ AQ2.js
@@ -0,0 +1,21 @@
+
+const prompt = require('prompt-sync') ()
+
+const size = prompt('Enter array size: ')
+let arr = []
+
+for ( i = 0; i < size; i++ ) {
+    arr.push( prompt('Enter a number: ') )
+}
+
+function replaceEven(size,arr) {
+    for ( i = 0; i < size; i++ ) {
+        if ( arr[i] %2 === 0 ) {
+            arr[i] = `$`
+        }
+        console.log( arr[i])
+    }   
+}
+
+replaceEven(size,arr)
+
