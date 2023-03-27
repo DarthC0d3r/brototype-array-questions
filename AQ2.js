b6032dabf2f2e8aa2723fd9ade4e6ff20010da08
Afsal <122087993+DarthC0d3r@users.noreply.github.com>
2023-03-27 11:10:33 +0530

    Update AQ2.js

--- AQ2.js
+++ AQ2.js
@@ -2,20 +2,22 @@
 const prompt = require('prompt-sync') ()
 
 const size = prompt('Enter array size: ')
-let arr = []
+const arr = []
 
-for ( i = 0; i < size; i++ ) {
-    arr.push( prompt('Enter a number: ') )
+console.log('Enter numbers:')
+
+for ( let i = 0; i < size; i++ ) {
+   arr[i] = prompt()
+   arr.map(Number)
 }
 
 function replaceEven(size,arr) {
-    for ( i = 0; i < size; i++ ) {
+    for ( let i = 0; i < size; i++ ) {
         if ( arr[i] %2 === 0 ) {
             arr[i] = `$`
         }
-        console.log( arr[i])
-    }   
+    }
+    console.log(`\n${arr}`)
 }
 
 replaceEven(size,arr)
-
